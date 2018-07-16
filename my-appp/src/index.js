24c47134898a01edc6399e9cd275a426b4e2d307
unknown <huangjian@ways.cn>
2018-07-16 09:36:22 +0800

    公司0716

--- my-appp/src/index.js
+++ my-appp/src/index.js
@@ -1,181 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-
-// class Square extends React.Component {
-// 	// 因为已经不需要保存state了，因此可以不要下面的代码，隐式创建constructor
-// 	// constructor(props) {
-// 	// 	super(props); // super作为函数时，代表父类的构造函数，但是this指向子类，
-// 	// 	// super(props)即相当于：React.Component.prototype.constructor.call(this, props);
-// 	// 	this.state = {
-// 	// 		value: null,
-// 	// 	};
-// 	// }
-	
-// 	render() {
-// 		return (
-// 			<button 
-// 				className="square"
-// 				onClick={() => this.props.onClick()}
-// 			>
-// 				{this.props.value}
-// 			</button>
-// 		);
-// 	}
-// }
-
-function Square(props) {
-	return (
-		<button className="square" onClick={props.onClick}>
-			{props.value}
-		</button>
-	)
-}
+import './index.css'
 
 
 class Board extends React.Component {
-	// constructor() {
-	// 	super(); // super作为函数时，代表父类的构造函数，但是this指向子类，
-	// 	// super(props)相当于：React.Component.prototype.constructor.call(this, props);
-	// 	this.state = {
-	// 		squares: Array(9).fill(null),
-	// 		xIsNext: true,
-	// 	};
-	// }
-
-
-	renderSquare(i) {
-		return (
-			<Square 
-				value={this.props.squares[i]}
-				onClick={() => this.props.onClick(i)}
-			/>
-		);
-	}
-
-	render() {
-		// const winner = calculateWinner(this.state.squares);
-		// let status;
-		// if (winner) {
-			// status = 'Winner: ' + winner;
-		// } else {
-			// status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-		// }
-
-		return (
-			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
-		);
-	}
+    renderSquare(i) {
+        return (
+            <square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        )
+    }
 }
 
 
 class Game extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			history: [{
-				squares: Array(9).fill(null),
-			}],
-			stepNumber: 0,
-			xIsNext: true,
-		};
-	}
-	
-	handleClick(i) {
-		// const history = this.state.history; // 浅拷贝
-		const history = this.state.history.slice(0, this.state.stepNumber + 1);
-		const current = history[history.length - 1]; // 浅拷贝
-		const squares = current.squares.slice(); // 深拷贝
+    constructor() {
+        super();
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
 
-		if (calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			history: history.concat([{
-				squares: squares
-			}]),
-			stepNumber: history.length,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1); 
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
 
-	jumpTo(step) {
-		this.setState({
-			stepNumber: step,
-			xIsNext: (step % 2) ? false :true,
-		});
-	}
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
 
-	render() {
-		const history = this.state.history;
-		const current = history[this.state.stepNumber];
-		const winner = calculateWinner(current.squares);
 
-		const moves = history.map((step, move) => {
-			const desc = move ? 'Move #' + move : 'Game start';
-			return (
-				<li key={move}>
-					<button onClick={() => this.jumpTo(move)}>{desc}</button>
-				</li>
-			)
-		})
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) ? false : true,
+        });
+    }
 
-		let status;
-		if (winner) {
-			status = 'Winner: ' + winner;
-		} else {
-			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-		}
 
-		return (
-			<div className="game">
-				<div className="game-board">
-					<Board 
-						squares={current.squares}
-						onClick={(i) => this.handleClick(i)}
-					/>
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
 
-				</div>
-				<div className="game-info">
-					<div>{status}</div>
-					<ol>{moves}</ol>
-				</div>
-			</div>
-		);
-	}
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Move #' + move : 'Game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
+            </div>
+        );
+    }
+
+
+
+
+
 }
 
-
-
-
-// ========================================
+// =======================================
 
 ReactDOM.render(
-	<Game />,
-	document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
 
 
 function calculateWinner(squares) {
-	const lines = [
+    const lines = [
 		[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
@@ -184,12 +115,12 @@ function calculateWinner(squares) {
 		[2, 5, 8],
 		[0, 4, 8],
 		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
--- test.js
+++ test.js
@@ -1,23 +1,3 @@
-var object = {
-	data: [1, 2, 3],
-	dataDouble: [1, 2, 3],
-	double: function() {
-		console.log("this inside of outerFn double()");
-		console.log(this);
-		return this.data.map(function(item) {
-			console.log(this); // 这里的this指什么
-			return item * 2;
-		});
-	},
-	doubleArrow: function() {
-		console.log("this inside of outerFn doubleArrow()");
-		console.log(this);
-		return this.dataDouble.map(item => {
-			console.log(this);
-			return item * 2;
-		});
-	}
-}
-
-object.double();
-object.doubleArrow();
+let arr = Array(10).fill();
+console.log(arr);
+console.log(arr.length);
